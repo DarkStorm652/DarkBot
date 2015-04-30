6e6a1066708b2cceb4aecf76f714e7b023977c2e
DarkStorm652 <darkstorm@evilminecraft.net>
2015-04-30 00:49:24 -0400

    Modify type option handling

--- core/src/main/java/org/darkstorm/minecraft/darkbot/protocol/type/AbstractType.java
+++ core/src/main/java/org/darkstorm/minecraft/darkbot/protocol/type/AbstractType.java
@@ -4,18 +4,25 @@ import java.util.*;
 
 abstract class AbstractType<T> implements Type<T> {
 	private final String name;
+	private final Map<String, Option<?>> optionsByName;
 	private final Map<Option<?>, ?> options;
 	
 	public AbstractType(String name, Map<Option<?>, Object> options) {
 		this.name = name;
-		
+
+		Map<String, Option<?>> optionsByName = new HashMap<>();
 		Map<Option<?>, Object> newOptions = new HashMap<>();
 		for(Map.Entry<Option<?>, ?> entry : options.entrySet()) {
 			Option<?> option = entry.getKey();
+			if(optionsByName.containsKey(option.getName()))
+				throw new IllegalArgumentException("Option name collision");
+			optionsByName.put(option.getName(), option);
+			
 			Object value = entry.getValue();
 			newOptions.put(option, option.getType().castValue(value));
 		}
-		this.options = Collections.unmodifiableMap(options);
+		this.optionsByName = Collections.unmodifiableMap(optionsByName);
+		this.options = Collections.unmodifiableMap(newOptions);
 	}
 
 	@Override
@@ -27,10 +34,10 @@ abstract class AbstractType<T> implements Type<T> {
 	public final Collection<Option<?>> getOptions() {
 		return options.keySet();
 	}
-
+	
 	@Override
-	public final boolean hasOption(Option<?> option) {
-		return options.containsKey(option);
+	public Option<?> getOption(String name) {
+		return optionsByName.get(name);
 	}
 
 	@Override
--- core/src/main/java/org/darkstorm/minecraft/darkbot/protocol/type/Type.java
+++ core/src/main/java/org/darkstorm/minecraft/darkbot/protocol/type/Type.java
@@ -3,13 +3,11 @@ package org.darkstorm.minecraft.darkbot.protocol.type;
 import java.io.*;
 import java.util.Collection;
 
-import org.darkstorm.minecraft.darkbot.protocol.Packet;
-
 public interface Type<T> {
 	public String getName();
 	public T castValue(Object value) throws IllegalArgumentException;
 	public Collection<Option<?>> getOptions();
-	public boolean hasOption(Option<?> option);
+	public Option<?> getOption(String name);
 	public <S> S getOptionValue(Option<S> option);
 
 	public T read(InputStream stream) throws IOException;
