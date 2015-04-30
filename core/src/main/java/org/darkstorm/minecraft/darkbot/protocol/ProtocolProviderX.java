1cd5a94d20587e96dc02c98b7a03db0a15ed1a7c
DarkStorm652 <darkstorm@evilminecraft.net>
2015-04-30 00:25:35 -0400

    Remove old protocol code

--- core/src/main/java/org/darkstorm/minecraft/darkbot/protocol/ProtocolProviderX.java
+++
@@ -1,9 +0,0 @@
-package org.darkstorm.minecraft.darkbot.protocol;
-
-import org.darkstorm.minecraft.darkbot.MinecraftBot;
-
-public abstract class ProtocolProviderX<T extends ProtocolX<?>> extends ProtocolProvider<T> {
-	@Override
-	public abstract T getProtocolInstance(MinecraftBot bot);
-
-}
--- core/src/main/java/org/darkstorm/minecraft/darkbot/protocol/ReadablePacket.java
+++
@@ -1,7 +0,0 @@
-package org.darkstorm.minecraft.darkbot.protocol;
-
-import java.io.*;
-
-public interface ReadablePacket extends Packet {
-	public void readData(DataInputStream in) throws IOException;
-}
