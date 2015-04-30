03233b1e71f6ac79fd44fd17da8c79562e64fef5
DarkStorm652 <darkstorm@evilminecraft.net>
2015-04-30 01:00:18 -0400

    Missing generics on ComparisonCondition

--- core/src/main/java/org/darkstorm/minecraft/darkbot/protocol/condition/ComparisonCondition.java
+++ core/src/main/java/org/darkstorm/minecraft/darkbot/protocol/condition/ComparisonCondition.java
@@ -3,6 +3,6 @@ package org.darkstorm.minecraft.darkbot.protocol.condition;
 import org.darkstorm.minecraft.darkbot.protocol.FieldModel;
 
 public interface ComparisonCondition extends Condition {
-	public FieldModel getField();
+	public FieldModel<?> getField();
 	public Object getValue();
 }
