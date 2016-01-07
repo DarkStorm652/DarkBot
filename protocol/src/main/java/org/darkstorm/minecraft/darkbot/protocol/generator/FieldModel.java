ab4705c89132b61e759a122a3ee33c2dd7697643
DarkStorm652 <darkstorm@evilminecraft.net>
2016-01-07 00:07:44 -0500

    Remove Condition from FieldModel

--- protocol/src/main/java/org/darkstorm/minecraft/darkbot/protocol/generator/FieldModel.java
+++ protocol/src/main/java/org/darkstorm/minecraft/darkbot/protocol/generator/FieldModel.java
@@ -2,14 +2,11 @@ package org.darkstorm.minecraft.darkbot.protocol.generator;
 
 import java.io.*;
 
-import org.darkstorm.minecraft.darkbot.protocol.generator.Field;
-import org.darkstorm.minecraft.darkbot.protocol.generator.condition.Condition;
 import org.darkstorm.minecraft.darkbot.protocol.generator.type.Type;
 
 public interface FieldModel<T> {
 	public String getName();
 	public Type<T> getType();
-	public Condition getCondition();
 	
 	public Field<T> read(InputStream stream) throws IOException;
 }
